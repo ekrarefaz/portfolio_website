98d2ef1dedc2ee2a1b9b78189ee90e14bedb17ef
ekrar <ekrar.efaz@gmail.com>
2021-04-27 17:12:20 +0600

    Dark and Light Mode Toggle Added with VanillaJS

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//console.dir(document);
-console.log(document.title);
+let themeDots = document.getElementsByClassName("theme-dot")
+for(var i=0; themeDots.length>i; i++){
+  themeDots[i].addEventListener('click', function(){
+    let mode = this.dataset.mode
+    console.log('option clicked', mode);
+    setTheme(mode);
+  })
+}
+function setTheme(mode){
+  if(mode == 'light'){
+    document.getElementById('theme-style').href = 'default.css'
+  }
+  else if (mode == 'dark'){
+    document.getElementById('theme-style').href = 'dark-mode.css'
+  }
+  else{
+    document.getElementById('theme-style').href = 'default.css'
+
+  }
+}
